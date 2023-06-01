6904a421a0ada76b3241c174936b4fbeab8c520f
Rhythm1821 <alamrawat1821@gmail.com>
2023-06-01 10:27:52 +0530

    'Ans-6'

---
+++ ans_6.py
@@ -0,0 +1,17 @@
+def search(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+print(search([-1,0,3,5,9,12],9))
+
